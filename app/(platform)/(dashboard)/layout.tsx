2c8a278e7100ce3b80b0624a823477970baa30f6
Fauzan <ojanaja03@gmail.com>
2023-11-20 21:30:31 +0700

    First Commit

---
+++ app/(platform)/(dashboard)/layout.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "./_components/navbar";
+
+const DashboardLayout = ({ children }: {
+    children: React.ReactNode;
+}) => {
+    return (
+        <div className="h-screen">
+            <Navbar/>
+            {children}
+        </div>
+    )
+}
+
+export default DashboardLayout
---
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,22 @@
+import { auth } from "@clerk/nextjs";
+import { OrgControl } from "./_components/org-control";
+import { startCase } from "lodash";
+
+export async function generateMetaData() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+}
+
+const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <OrgControl />
+      {children}
+    </>
+  );
+};
+
+export default OrganizationIdLayout;
